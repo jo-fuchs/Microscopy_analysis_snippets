9e19afe478f6467d75afa99dbd830991d1356642
FuchsJo <joachim.fuchs@yahoo.de>
2020-11-03 12:28:35 +0100

    no accidental identical names

--- R/Randomize_folder.R
+++ R/Randomize_folder.R
@@ -3,32 +3,34 @@
 ## Original files are moved to "Originals" subfolder
 
 ## caution: randomizes all files in the specified folder, irrespective of file-type > clean up before use
+## Version 0.2 : random name without replacement > no accidentally identical names
 
-randomize_folder <- function() {
+
+randomize_folder<- function() {
   # General settings & libraries
   rm(list = ls()) # Clear workspace
   library(tools)
   library(tcltk)
-
+  
   setwd("~")
   dir <- tclvalue(tkchooseDirectory())
-
+  
   setwd(dir)
   files <- data.frame(NA, list.files(dir))
   colnames(files) <- c("Random_Name", "Original_Name")
   dir.create("Originals")
-
+  index <- sample(1:nrow(files), nrow(files), replace = F)
+  
   for (i in 1:nrow(files)) {
     file.copy(paste(dir, "\\", files[i, 2], sep = ""), paste(dir, "\\Originals", sep = ""))
-    temp <- round(runif(1, 0, 100))
     ex <- file_ext(files[i, 2])
-    temp <- paste(temp, ex, sep = ".")
+    temp <- paste(index[[i]], ex, sep = ".")
     file.rename(paste(dir, "\\", files[i, 2], sep = ""), paste(dir, "\\", temp, sep = ""))
     files[i, 1] <- temp
   }
-
-
+  
+  
   write.table(files, "Randomization.csv", row.names = F, quote = F, sep = ";", dec = ".")
 }
 
-randomize_folder()
+File_randomizer()
